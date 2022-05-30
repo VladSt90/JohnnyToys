7fe27ba6642f59b3161d2b099540cf09d5905a97
Vladislav Stukov <>
2022-05-30 12:27:24 +0400

    Setup react navigation

--- android/app/src/main/java/com/johnnytoys/MainActivity.java
+++ android/app/src/main/java/com/johnnytoys/MainActivity.java
@@ -3,6 +3,7 @@ package com.johnnytoys;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
 
@@ -13,6 +14,11 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "JohnnyToys";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 
   /**
---
+++ src/app.js
@@ -0,0 +1,13 @@
+import {NavigationContainer} from '@react-navigation/native';
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export const App = () => {
+  return (
+    <NavigationContainer>
+      <View>
+        <Text>Hello world</Text>
+      </View>
+    </NavigationContainer>
+  );
+};
